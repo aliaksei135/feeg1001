a9adc8733c85fc7c6ca5a916a7b70e7d56e8b741
Aliaksei Pilko <aliakseipilko1@gmail.com>
2017-10-27 19:50:18 +0100

    Complete lab4

--- lab4.py
+++ lab4.py
@@ -5,8 +5,8 @@ Created on Mon Oct 23 13:17:49 2017
 @author: ap9g17
 """
 
-import urllib
 import re
+import urllib
 
 
 # Provided
@@ -22,30 +22,43 @@ def line_averages(filename):
     with open(filename, 'rt') as f:
         avgs = []
         for line in f:
-            numbers = line.split(',')
-            numbers = list(map(int, numbers))
+            numbers = line.strip().split(',')
+            numbers = list(map(float, numbers))
             avg = 0
             for n in numbers:
                 avg += n
-            avg = avg/len(numbers)
+            avg = avg / len(numbers)
             avgs.append(avg)
+        f.close()
         return avgs
 
 
 def noaa_temperature(s):
-    """extract the temperature in degree Celsius from the string, 
+    """extract the temperature in degree Celsius from the string,
     and return this temperature as an integer number"""
-    temp_str = re.findall('((\d){2}(?= C))', s)[0]
-    return int(temp_str[0])
+    temp_strs = re.findall(
+        r'(?<=Temperature:\s[0-9]{2}\s\w\s\()((-)?[0-9]+)', s.strip())[0]
+    if (len(temp_strs) > 0 and len(temp_strs[0]) > 0):
+        return int(temp_strs[0])
+    else:
+        return []
 
 
 def seq_sqrt(xs):
-    pass
+    """ takes a list of non-negative numbers xs with elements
+    [x0, x1, x2, ..., xn], and returns the list
+    [sqrt(x0), sqrt(x1), sqrt(x2), ..., sqrt(xn)]"""
+    return [x ** 0.5 for x in xs]
 
 
 def mean(xs):
-    pass
+    """takes a sequence xs of numbers, and returns the (arithmetic) mean """
+    return sum(xs) / len(xs)
 
 
 def wc(filename):
-    pass
+    """returns the number of words in file filename"""
+    with open(filename, 'rt') as f:
+        words = re.findall('(\w+)', f.read())
+        f.close()
+        return len(words)
